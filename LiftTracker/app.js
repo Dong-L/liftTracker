63694efde9918648d645a85c2a12ccba5837a431
Dong Lee <35810705+Dong-L@users.noreply.github.com>
2024-05-25 02:50:37 -0400

    Add files via upload

--- LiftTracker/app.js
+++ LiftTracker/app.js
@@ -1,10 +1,13 @@
 // create constants for the form and the form controls
-const newPeriodFormEl = document.getElementsByTagName("form")[0];
+const newLiftFormEl = document.getElementsByTagName("form")[0];
 const startTimeInputEl = document.getElementById("start-time");
 const endTimeInputEl = document.getElementById("end-time");
+const pastLiftContainer = document.getElementById("past-lifts");
+// Add the storage key as an app-wide constant
+const STORAGE_KEY = "lift-tracker";
 
 // Listen to form submissions.
-newPeriodFormEl.addEventListener("submit", () => {
+newLiftFormEl.addEventListener("submit", () => {
     // Prevent the form from submitting to the server
     // since everything is client-side
     preventDefault();
@@ -14,8 +17,8 @@ newPeriodFormEl.addEventListener("submit", () => {
     const endTime = endTimeInputEl.value;
 
     // Check if the times are invalid
-    if (checkDatesInvalid(startTime, endTime)) {
-        // If the dates are invalid, exit.
+    if (checkTimesInvalid(startTime, endTime)) {
+        // If the times are invalid, exit.
         return;
     }
 
@@ -28,3 +31,86 @@ newPeriodFormEl.addEventListener("submit", () => {
     // Reset the form.
     newLiftFormEl.reset();
 });
+
+function checkTimesInvalid(startTime, endTime) {
+    // Check that end time is after start time and neither are null
+    if (!startTime || !endTime || startTime > endTime) {
+        /*  To make the validation robust we could:
+            1. add error messaging based on error type
+            2. Alert assistive technology users about the error
+            3. move focus to the error location
+            instead, for now, we clear the times if either or both are invalid */
+        newLiftFormEl.reset();
+        // as times are invalid, we return true
+        return true;
+    }
+    return false;
+}
+
+function storeNewLift(startTime, endTime) {
+    // Get data from storage
+    const lifts = getAllStoredLifts();
+
+    // Add the new lift object to the end of the array of lift objects
+    lifts.push({ startTime, endTime });
+
+    // Sort the array so that lifts are ordered by start time, from newest to oldest
+    lifts.sort((a, b) => {
+        return new Date(b.startTime) - new Date(a.startTime);
+    });
+
+    // Store the updated array back in storage
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(lifts));
+}
+
+function getAllStoredLifts() {
+    // Get the string lifts from localStorage
+    const data = window.localStorage.getItem(STORAGE_KEY);
+
+    // If no lifts were stored, default to an empty array
+    // otherwise, return the stored data as parse JSON
+    const lifts = data ? JSON.parse(data) : [];
+    console.dir(lifts);
+    console.log(lifts);
+    return lifts;
+}
+
+function renderPastLifts() {
+    const pastLiftHeader = document.createElement("h2");
+    const pastLiftList = document.createElement("ul");
+    // get the parse string of lifts, or an empty array.
+    const lifts = getAllStoredLifts();
+
+    // exit if there are no lifts
+    if (lifts.length === 0) {
+        return;
+    }
+
+    // Clear the list of past lifts, since we're going to re-render it.
+    pastLiftContainer.innerHTML = "";
+
+    pastLiftHeader.textContent = "Past lifts";
+
+    // Loop over all lifts and render them.
+    lifts.forEach((lift) => {
+        const liftEl = document.createElement("li");
+        liftEl.textContent = `From ${formatTime(
+            lift.startTime,
+        )} to ${formatTime(lift.endTime)}`;
+        pastLiftList.appendChild(liftEl);
+    });
+
+    pastLiftContainer.appendChild(pastLiftHeader);
+    pastLiftContainer.appendChild(pastLiftList);
+}
+
+function formatTime(timeString) {
+    // Convert the time string to Time object
+    const time = new Date(timeString);
+
+    // Format the time into a locale-specific string.
+    // include your locale for better user experience
+    return time.toLocalTimeString("en-US", { timeZone: "EST" });
+}
+
+renderPastLifts();
